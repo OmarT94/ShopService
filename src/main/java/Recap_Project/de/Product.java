fdfc714d302eae48221753a1a9b2058bdc4ba8f9
OmarT94 <93581484+OmarT94@users.noreply.github.com>
2024-12-02 15:42:08 +0100

    Set_up ProductRepo

---
+++ src/main/java/Recap_Project/de/Product.java
@@ -0,0 +1,5 @@
+package Recap_Project.de;
+
+public record Product(int id, String name, int price, String description) {
+
+}
---
+++ src/main/java/Recap_Project/de/ProductRepo.java
@@ -0,0 +1,33 @@
+package Recap_Project.de;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepo {
+    private final List<Product>products;
+
+    public ProductRepo() {
+        this.products = new ArrayList<Product>();
+    }
+
+    public void add(Product product) {
+        products.add(product);
+    }
+
+    public boolean remove(int productId) {
+        return products.removeIf(product -> product.id()==productId );
+    }
+
+    public Product get(int productId) {
+        return products.stream().filter(product -> product.id()==productId).findFirst().orElse(null);
+    }
+
+    public List<Product> getAll() {
+        return new ArrayList<>(products);
+    }
+
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
